d9011129fb51b9cf58a2dd60f37cf8de3d033055
KasunjGithub <167292427+KasunjGithub@users.noreply.github.com>
2024-04-21 15:21:30 +0530

    Add files via upload

---
+++ ComboBox/ComboBox/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblsname = new System.Windows.Forms.Label();
+            this.lblsid = new System.Windows.Forms.Label();
+            this.lbladdress = new System.Windows.Forms.Label();
+            this.lblcontact = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "StudentDetails";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Student Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(55, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Student ID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(55, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Address:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(55, 308);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 19);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Contact Number:";
+            // 
+            // lblsname
+            // 
+            this.lblsname.AutoSize = true;
+            this.lblsname.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsname.Location = new System.Drawing.Point(198, 188);
+            this.lblsname.Name = "lblsname";
+            this.lblsname.Size = new System.Drawing.Size(49, 19);
+            this.lblsname.TabIndex = 6;
+            this.lblsname.Text = "*****";
+            // 
+            // lblsid
+            // 
+            this.lblsid.AutoSize = true;
+            this.lblsid.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsid.Location = new System.Drawing.Point(198, 232);
+            this.lblsid.Name = "lblsid";
+            this.lblsid.Size = new System.Drawing.Size(49, 19);
+            this.lblsid.TabIndex = 7;
+            this.lblsid.Text = "*****";
+            // 
+            // lbladdress
+            // 
+            this.lbladdress.AutoSize = true;
+            this.lbladdress.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbladdress.Location = new System.Drawing.Point(198, 269);
+            this.lbladdress.Name = "lbladdress";
+            this.lbladdress.Size = new System.Drawing.Size(49, 19);
+            this.lbladdress.TabIndex = 8;
+            this.lbladdress.Text = "*****";
+            // 
+            // lblcontact
+            // 
+            this.lblcontact.AutoSize = true;
+            this.lblcontact.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcontact.Location = new System.Drawing.Point(198, 308);
+            this.lblcontact.Name = "lblcontact";
+            this.lblcontact.Size = new System.Drawing.Size(49, 19);
+            this.lblcontact.TabIndex = 9;
+            this.lblcontact.Text = "*****";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 416);
+            this.Controls.Add(this.lblcontact);
+            this.Controls.Add(this.lbladdress);
+            this.Controls.Add(this.lblsid);
+            this.Controls.Add(this.lblsname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblsname;
+        private System.Windows.Forms.Label lblsid;
+        private System.Windows.Forms.Label lbladdress;
+        private System.Windows.Forms.Label lblcontact;
+    }
+}
+
---
+++ ComboBox/ComboBox/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox
+{
+    public partial class Form1 : Form
+    {
+        DataTable stu = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //call the method of data table
+            studentdetails();
+            //pass the data into the combo box
+            comboBox1.DataSource= stu;
+            //display the student name in combobox for the selection
+            comboBox1.DisplayMember = "STNAME";
+        }
+        private void studentdetails()
+        {
+            //add columns for data table
+            stu.Columns.Add("STNAME");
+            stu.Columns.Add("STID",typeof(int));
+            stu.Columns.Add("ADDRESS");
+            stu.Columns.Add("CONTACTNUMBER");
+
+            stu.Rows.Add("Adams",96,"Jffna",0778065298);
+            stu.Rows.Add("Jane", 92, "Puttalam", 0723289206);
+            stu.Rows.Add("Tom", 89, "Badhulla", 07592069823);
+            stu.Rows.Add("Lara", 83, "Hatton", 0716508928);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblsname.Text = stu.Rows[comboBox1.SelectedIndex]["STNAME"].ToString();
+            lblsid.Text = stu.Rows[comboBox1.SelectedIndex]["STID"].ToString();
+            lbladdress.Text = stu.Rows[comboBox1.SelectedIndex]["ADDRESS"].ToString();
+            lblcontact.Text = stu.Rows[comboBox1.SelectedIndex]["CONTACTNUMBER"].ToString();
+        }
+    }
+}
